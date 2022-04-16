9c5214330f6ff093a217582637920fe87386d9d9
damon <61207844+damonot@users.noreply.github.com>
2022-04-16 16:10:22 -0400

    style: org

--- node_react_app/client/src/component/GitAPI.js
+++ node_react_app/client/src/component/GitAPI.js
@@ -1,30 +1,35 @@
 import { useEffect, useState } from "react"
-
+import verified from '../static/verified.png'
+import unverified from '../static/unverified.png'
+import moment from 'moment'
 const API = 'https://api.github.com/orgs/'
 
 const GitAPI = ({props}) => {
-    const [valid, setValidity] = useState('');
-    const [id, setId] = useState ('');
-    const [name, setName] = useState ('');
-    const [html_url, setHtml_url] = useState ('');
-    const [is_verified, setIs_verified] = useState ('');
-    const [created_at, setCreated_at] = useState ('');
-    const [updated_at, setUpdated_at] = useState ('');
+    const [valid, setValidity] = useState(true);
+    const [id, setId] = useState (6969696);
+    const [name, setName] = useState ('Amazon Web Services');
+    const [html_url, setHtml_url] = useState ('https://github.com/aws');
+    const [avatar_url, setAvatar_url] = useState ('https://avatars.githubusercontent.com/u/2232217?v=4');
+    const [is_verified, setIs_verified] = useState (true);
+    const [created_at, setCreated_at] = useState ('2012-08-28T05:37:55Z');
+    const [updated_at, setUpdated_at] = useState ('2022-03-18T18:37:08Z');
+    const friendlyCreate = moment(created_at).fromNow();
+    const friendlyUpdate = moment(updated_at).fromNow();
     const url = API+props;
 
-    useEffect(() => {
-        console.log("Requested "+ props )
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                if(data.hasOwnProperty('id')) {
-                    setValidity(true)
-                    setData(data)
-                } else setValidity(false)
-            }).catch(err => {
-                console.log(err);
-            });
-    });
+    // useEffect(() => {
+    //     console.log("Requested "+ props )
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             if(data.hasOwnProperty('id')) {
+    //                 setValidity(true)
+    //                 setData(data)
+    //             } else setValidity(false)
+    //         }).catch(err => {
+    //             console.log(err);
+    //         });
+    // });
 
     const setData = ({
         id,
@@ -41,15 +46,39 @@ const GitAPI = ({props}) => {
         setUpdated_at(updated_at);
     }
 
+
     if(valid) {
         return(
-            <div>{name} {id}</div>
+            <section className="container org">
+                <div className="flex align">
+                    <a href={html_url}><img src={avatar_url}></img></a>
+                    <h2><a href={html_url}>{name}</a></h2>
+                    {is_verified ? 
+                        <img className="verify safe" src={verified} alt="verified" title="Verifed"></img> 
+                        : <img className="verify danger" src={unverified} alt="unverified" title="Unverifed"></img>}
+                </div>
+                <div className="flex wrap push-up">
+                    <h3 title="ID">#{id}</h3>
+
+                    {/* Has Updated? */}
+                    { created_at < updated_at ? 
+                        <div className="flex wrap">
+                            <h3 className="highlight">Modified: {friendlyUpdate}</h3>
+                            <h3>Made: {friendlyCreate}</h3>
+                        </div>
+                        : <div className="flex wrap">
+                            <h3>Modified: {friendlyUpdate}</h3>
+                            <h3 className="highlight">Made: {friendlyCreate}</h3>
+                        </div>
+                    }
+                </div>
+            </section>
         )
     } else {
         return(
             <div>
-                <div>We couldn't find that org :/</div>
-                <h2>(note: you may have reached the Github API request limit)</h2>
+                <h1>We couldn't find that org :/</h1>
+                <h3>(note: you may have reached the Github API request limit)</h3>
             </div>
         )
     }
