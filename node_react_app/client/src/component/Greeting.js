b8a9dd41e74cc6c6c9d370888ccc0bf267128978
damon <61207844+damonot@users.noreply.github.com>
2022-04-16 16:59:40 -0400

    style: centering

--- node_react_app/client/src/component/Greeting.js
+++ node_react_app/client/src/component/Greeting.js
@@ -2,7 +2,7 @@ const Greeting = () => {
     return(
         <div className="container push-down">
             <h1>Who are you looking for?</h1>
-            <h4 style={{margin:"auto"}}>enter your org name below, e.g. 'boomtownroi' or 'google'</h4>
+            <h4 style={{maxWidth:"75%"}}>enter your org name below, e.g. 'boomtownroi' or 'google'</h4>
         </div>
     )
 }
