e053dccb1acb4b9c1eb5bd1e35c7500f6f17eccb
damon <61207844+damonot@users.noreply.github.com>
2022-04-17 04:24:40 -0400

    feat: error-handling repos

--- node_react_app/client/src/component/Repos.js
+++ node_react_app/client/src/component/Repos.js
@@ -4,7 +4,7 @@ import repos from "../development/repos"
 
 const Repos = ({props}) => {
     const [data, setData] = useState([])
-    const [valid, setValid] = useState([])
+    const [valid, setValidity] = useState()
     const URL = props+"/repos"
     let friendlyCreate;
     let friendlyUpdate;
@@ -14,8 +14,11 @@ const Repos = ({props}) => {
         fetch(URL)
         .then(res => res.json())
         .then(json => {
-            console.log(json);
-            handleData(json);
+            console.log(json[0]);
+            if(json[0].hasOwnProperty('id')) {
+                setValidity(true);
+                handleData(json);
+            } else setValidity(false);
         })
         .catch(err => console.log(err));
     });
@@ -24,49 +27,60 @@ const Repos = ({props}) => {
         setData(json)
     }
 
-    return(
-        <div className="repos" id="repos">
-            {/* <h2 className=" allcaps"style={{textAlign:"center"}}>repos</h2> */}
-            <div className="mx flex wrap">
-                <div className="scrollable-wrapper">
-                <table className="table">
-                    <thead>
-                        <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Language</th>
-                        <th>Pushed</th>
-                        <th>Made</th>
-                        <th>Modified</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            data.map(i => {
-                                friendlyCreate = moment(i.created_at).fromNow();
-                                friendlyUpdate = moment(i.updated_at).fromNow();
-                                friendlyPush = moment(i.pushed_at).fromNow();
-                                return (
-                                    <tr key={i.id}>
-                                        <td data-label="ID">{i.id}</td>
-                                        <td data-label="Name"> <a href={i.html_url}>{i.name}</a></td>
-                                        <td data-label="Description">{i.description}</td>
-                                        <td data-label="Language">{i.language}</td>
-                                        <td data-label="Pushed">{friendlyPush}</td>
-                                        <td data-label="Made">{friendlyCreate}</td>
-                                        <td data-label="Modified">{friendlyUpdate}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
+    if(!valid) {
+        return(
+            <div className="repos" id="repos">
+                {/* <h2 className=" allcaps"style={{textAlign:"center"}}>repos</h2> */}
+                <div className="mx flex wrap">
+                    
+                    <div className="scrollable-wrapper">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Language</th>
+                            <th>Pushed</th>
+                            <th>Made</th>
+                            <th>Modified</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map(i => {
+                                    friendlyCreate = moment(i.created_at).fromNow();
+                                    friendlyUpdate = moment(i.updated_at).fromNow();
+                                    friendlyPush = moment(i.pushed_at).fromNow();
+                                    return (
+                                        <tr key={i.id}>
+                                            <td data-label="ID">{i.id}</td>
+                                            <td data-label="Name"> <a href={i.html_url}>{i.name}</a></td>
+                                            <td data-label="Description">{i.description}</td>
+                                            <td data-label="Language">{i.language}</td>
+                                            <td data-label="Pushed">{friendlyPush}</td>
+                                            <td data-label="Made">{friendlyCreate}</td>
+                                            <td data-label="Modified">{friendlyUpdate}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                    </div>
                 </div>
             </div>
-        </div>
-        
-    )
+            
+        )
+    } else {
+        return (
+            <div className="box mx">
+                <h3>Repositorites</h3>
+                <p>none found :(</p>
+            </div>
+        )
+    }
+    
 }
 
 export default Repos
