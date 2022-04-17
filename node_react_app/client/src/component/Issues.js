8653234229de7234889e8a1a9e7b1a1c1386313d
damon <61207844+damonot@users.noreply.github.com>
2022-04-17 13:46:20 -0400

    feat: dummy issues

---
+++ node_react_app/client/src/component/Issues.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import issues from "../development/issues";
+
+const Issues = ({props}) => {
+    const [data, setData] = useState([])
+    const [valid, setValidity] = useState(true)
+    const URL = props+"/issues"
+    let friendlyCreate;
+    let friendlyUpdate;
+    let friendlyPush;
+
+    // useEffect(() => {
+    //     fetch(URL)
+    //     .then(res => res.json())
+    //     .then(json => {
+    //         if(json[0] !== undefined && json.hasOwnProperty('id')) {
+    //             setValidity(true);
+    //             handleData(json);
+    //         } else setValidity(false);
+    //     })
+    //     .catch(err => console.log(err));
+    // });
+
+    // const handleData = (json) => {
+    //     setData(json)
+    // }
+
+    if(valid) {
+        return(
+            <div className="shrink" id="issues">
+                <div className="mx flex wrap box">
+                    <h3 className="stretch">Issues</h3>
+                    <div className="scrollable-wrapper">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                            <th>ID</th>
+                            <th>User</th>
+                            <th>Number</th>
+                            <th>State</th>
+                            <th>Title</th>
+                            <th>Body</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                issues.map(i => {
+                                    return (
+                                        <tr key={i.id}>
+                                            <td data-label="ID">{i.id}</td>
+                                            <td data-label="User"> <a href={i.user.url}> <img src={i.avatar_url}alt={i.user.login}></img> </a></td>
+                                            <td data-label="Number"><a href={i.url}>{i.number}</a></td>
+                                            <td data-label="State">{i.state}</td>
+                                            <td data-label="Title">{i.title}</td>
+                                            <td data-label="Body">{i.body}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div>
+            
+        )
+    } else {
+        return (
+            <div className="box mx fitcontent">
+                <h3>Issues</h3>
+                <p>none found :(</p>
+            </div>
+        )
+    }
+    
+}
+
+export default Issues
+
